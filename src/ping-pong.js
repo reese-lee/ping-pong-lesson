3324ef9d813e45cd2d5f74036888053613e40dd7
reese-lee <reeselee.cy@gmail.com>
2019-11-12 15:29:59 -0800

    add function for pingPong

---
+++ src/ping-pong.js
@@ -0,0 +1,20 @@
+export default function pingPong(goal) {
+  var output = [];
+  for (var i = 1; i <= goal; i++) {
+    if (i % 15 === 0) {
+      output.push("ping-pong");
+    } else if (i % 3 === 0) {
+      output.push("ping");
+    } else if (i % 5 === 0) {
+      output.push("pong");
+    } else  {
+      output.push(i);
+    }
+  }
+  return output;
+}
+
+// export function sup()
+// {
+//   console.log(sup);
+// }
